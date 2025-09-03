5b46118f38d88e1c4aae0b0195f0f64d9438c454
Art Vardanyan <akvardanyan99@gmail.com>
2025-09-03 10:58:20 -0400

    def handle_cover_letter_pdf

--- clean-downloads-folder.py
+++ clean-downloads-folder.py
@@ -2,6 +2,9 @@ import logging
 from pathlib import Path
 import os
 import shutil
+import time
+from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
 
 DOWNLOADS = Path.home() / "Downloads"
 FOLDERS = {
@@ -41,6 +44,23 @@ def safe_move(src: Path, dest_folder_name: str) -> Path:
     logging.info(f"Moved {src.name} -> {dest}")
     return dest
 
+def handle_cover_letter_pdf(path: Path):
+    for f in DOWNLOADS.glob("*"):
+        try:
+            if f.is_file() and SPECIAL_CL_NAME in f.name and f != path:
+                f.unlink()
+                logging.info(f"Deleted old cover letter: {f.name}")
+        except Exception as e:
+            logging.warning(f"Failed to delete {f.name}: {e}")
+
+    target = DOWNLOADS / f"{SPECIAL_CL_NAME}.pdf"
+    if target.exists() and target!=path:
+        try:
+            target.unlink()
+            logging.info(f"Removed existing {target.name} to enforce exact name")
+        except Exception as e:
+            logging.error(f"Rename to '{target.name} failed: {e}")
+
 def categorize_file(path: Path):
     if not path.is_file():
         return
@@ -48,7 +68,10 @@ def categorize_file(path: Path):
 
     # PDFs
     if ext == ".pdf":
-        safe_move(path, "PDFs")
+        if SPECIAL_CL_NAME in path.name:
+            handle_cover_letter_pdf()
+        else:
+            safe_move(path, "PDFs")
         return
     
     # recognized non-PDFs
@@ -65,6 +88,25 @@ def scan_existing_files():
         if p.is_file():
             categorize_file(p)
 
+class DownloadsHandler(FileSystemEventHandler):
+    def on_created(self, event):
+        time.sleep(2)
+        path = Path(event.src_path)
+        categorize_file(path)
+
 if __name__ == "__main__":
     ensure_folders()
-    logging.info(f"Helpers ready. Downloads path: {DOWNLOADS}")
+    scan_existing_files()
+
+    observer = Observer()
+    observer.schedule(DownloadsHandler(), str(DOWNLOADS), recursive=False)
+    observer.start()
+    
+    logging.info(f"Watching folder: {DOWNLOADS}")
+
+    try:
+        while True:
+            time.sleep(5)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
